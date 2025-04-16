e4942d4cdb4a42f6f3fd71b6ba5123e654515c27
ritik <ritikstm16@gmail.com>
2025-04-16 18:08:22 +0530

    first commit

---
+++ src/main/java/net/javaguides/expansetrackerapp/ExpanseTrackerAppApplication.java
@@ -0,0 +1,13 @@
+package net.javaguides.expansetrackerapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExpanseTrackerAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExpanseTrackerAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/net/javaguides/expansetrackerapp/ExpanseTrackerAppApplicationTests.java
@@ -0,0 +1,13 @@
+package net.javaguides.expansetrackerapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpanseTrackerAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
